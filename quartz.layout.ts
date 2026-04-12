4b2b1837343e775bd6971dcc2bf12d4e3c576341
Tony <lightning_tony@protonmail.com>
2026-04-12 10:21:02 +0400

    upd: remove duplicate headers

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.PageTitle(), Component.Spacer()],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
